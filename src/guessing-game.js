d5b280f374e8f6969439d3c4b820a11ee351fa4f
Lizaveta <lizadondysh@icloud.com>
2022-08-04 23:03:14 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.item = Math.round((this.min + this.max) / 2);
+        return this.item;
     }
 
     lower() {
-
+        this.max = this.item;
     }
 
     greater() {
-
+        this.min = this.item;
     }
 }
 
